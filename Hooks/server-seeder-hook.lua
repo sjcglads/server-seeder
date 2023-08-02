631db3bde35f82b71d372880fa680ab305ed6fa6
sjcglads <{ID}+{username}@users.noreply.github.com>
2023-08-01 20:26:09 -0400

    Add sample dofile line

--- Hooks/server-seeder-hook.lua
+++ Hooks/server-seeder-hook.lua
@@ -5,3 +5,9 @@
 major_version = 0
 minor_version = 0
 build_number = 1
+
+currentVersion = major_version .. '.' .. minor_version .. '.' .. build_number
+
+-- use dofile function to launch the mod from lua files in the net folder
+-- something like this: 
+dofile(lfs.writedir() .. '../net/Server-Seeder-' .. currentVersion .. '/main-loop.lua')
